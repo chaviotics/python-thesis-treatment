93dce5d3de055a6d80d56a7c7c565bff41580f45
Chav Sumalinog <chaviotics@gmail.com>
2023-05-26 11:22:31 +0800

    Updated List page to use the interactive code editors

--- client/src/pages/Lists/Lists.jsx
+++ client/src/pages/Lists/Lists.jsx
@@ -38,81 +38,61 @@ function Lists() {
         />
 
         {/* Example */}
-        <Typography mt="4rem">
+        <Typography mt="4rem" mb="1rem">
           For an example, let's make a list of fruits and place in a variable
-          called <code>fruits</code>. It would look like this:
+          called <code>fruits</code>. Press <code>RUN CODE</code> to check the
+          output!
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`fruits = ["apple", "banana", "cherry"]\n\nprint(fruits)\n`}
-            height="120px"
-            width="60%"
-            title="true"
-          />
+        <CodeEditorWithOutput
+          content={`fruits = ["apple", "banana", "cherry"]
 
-          <CodeOutput
-            content={`['apple', 'banana', 'cherry']`}
-            height="120px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+# Try printing out "fruits" list
+`}
+          codeWidth={60}
+          outputWidth={40}
+          height="150px"
+          title="true"
+        />
 
-        <Typography mt="4rem">
-          The example below shows a lists of single data type elements:
+        <Typography mt="4rem" mb="1rem">
+          Earlier, we created a list that houses values that are strings. The
+          example below shows lists of the values of other data types:
         </Typography>
 
-        <CodeEditor
+        <CodeEditorWithOutput
           content={`list_of_numbers = [1, 3, 420, 69, 5]
 list_of_floats = [1.0, 1.1, 6.9, 4.20, 42.0]
 list_of_booleans = [True, False, False, True, True]
+
+print(list_of_numbers)
+print(list_of_floats)
+print(list_of_booleans)
 `}
-          height="120px"
-          width="100%"
+          codeWidth={60}
+          outputWidth={40}
+          height="250px"
+          title="true"
         />
 
-        <Typography mt="4rem">
+        <Typography mt="4rem" mb="1rem">
           However, we can also have a list of different data type elements like
           this:
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`mixed_list = [1, "Isaac", 3.69, True]
+        <CodeEditorWithOutput
+          content={`mixed_list = [1, "Isaac", 3.69, True]
 
 print(mixed_list)
 `}
-            height="120px"
-            width="60%"
-            title="true"
-          />
-          <CodeOutput
-            content={`[1, 'Isaac', 3.69, True]`}
-            height="120px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+          codeWidth={60}
+          outputWidth={40}
+          height="150px"
+          title="true"
+        />
 
         {/* List Order Example */}
-        <Typography mt="4rem" mb="4rem">
+        <Typography mt="4rem" mb="2rem">
           Lists are ordered, which means that the items have a defined order and
           can be accessed using an index, starting from 0 for the first item.
           The syntax for accessing a specific item in a list is:{' '}
@@ -122,189 +102,135 @@ print(mixed_list)
         <Typography mb="1rem">
           Let's say we want to choose the first element of the{' '}
           <code>fruits</code> list, which is <code>apple</code>. Take note that
-          the index of the first element is <code>0</code> of the list. We would
-          do it like this:
+          the index of the first element is <code>0</code> of the list. Try out
+          printing the 3rd element of the list!
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`fruits = ["apple", "banana", "cherry"]\n\nprint(fruits[0])\n`}
-            height="120px"
-            width="60%"
-            title="true"
-          />
+        <CodeEditorWithOutput
+          content={`fruits = ["apple", "banana", "cherry"]
 
-          <CodeOutput
-            content={`apple`}
-            height="120px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+print(fruits[0])
+
+# Try out printing the 3rd element of the list
+`}
+          codeWidth={70}
+          outputWidth={30}
+          height="250px"
+          title="true"
+        />
 
         {/* List Mutable Example */}
 
         <Typography mt="4rem" mb="1rem">
           Lists are mutable, which means that you can change the items in a list
           after it has been created. In this case, we are changing the 2nd
-          element, <code>banana</code>, to <code>orange</code>.
+          element, <code>banana</code>, to <code>orange</code>. Also, do the
+          mini-activity below!
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`fruits = ["apple", "banana", "cherry"]
+        <CodeEditorWithOutput
+          content={`fruits = ["apple", "banana", "cherry"]
 
 fruits[1] = "orange"
 print(fruits)
-`}
-            height="150px"
-            width="60%"
-            title="true"
-          />
 
-          <CodeOutput
-            content={`['apple', 'orange', 'cherry']`}
-            height="150px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+# ----- MINI-ACTIVITY -----
+
+# Try changing the 1st element in the list to "mango"
+
+
+# Print the 1st element in the list
+
+`}
+          codeWidth={70}
+          outputWidth={30}
+          height="360px"
+          title="true"
+        />
 
         {/* append() */}
         <Typography mt="4rem" mb="1rem">
           Items can be added in the list by using the <code>append()</code>{' '}
           method. Here, we are adding <code>grapes</code> to our{' '}
           <code>fruits</code> list containing <code>apple</code> and{' '}
-          <code>banana</code>.
+          <code>banana</code>. Do the mini-activity below!
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`fruits = ["apple", "banana"]
+        <CodeEditorWithOutput
+          content={`fruits = ["apple", "banana"]
 
 fruits.append("grapes")
 print(fruits)
-`}
-            height="160px"
-            width="60%"
-            title="true"
-          />
 
-          <CodeOutput
-            content={`['apple', 'banana', 'grapes']`}
-            height="160px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+# ----- MINI-ACTIVITY ----- 
+
+# Add "lemon" to the "fruits" list using append()
+
+
+# Print out the list
+`}
+          codeWidth={70}
+          outputWidth={30}
+          height="350px"
+          title="true"
+        />
 
         {/* len() */}
-        <Typography mt="2rem" mb="1rem">
+        <Typography mt="4rem" mb="1rem">
           You can get the size of the list (how many items in the list) by using
           the <code>len()</code> method. You just place the <code>fruits</code>{' '}
           list inside the <code>len()</code> method, and place that inside a{' '}
           <code>print()</code> to see its output.
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`fruits = ["apple", "banana"]
+        <CodeEditorWithOutput
+          content={`fruits = ["apple", "banana"]
 print(len(fruits)) # Output: 2
 
 fruits.append("grapes")
-print(len(fruits)) # Output: 3
-`}
-            height="200px"
-            width="60%"
-            title="true"
-          />
 
-          <CodeOutput
-            content={`2\n3`}
-            height="200px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+# Print out the length of fruits now!
+`}
+          codeWidth={70}
+          outputWidth={30}
+          height="250px"
+          title="true"
+        />
 
         {/* List Operations */}
-        <Typography mt="2rem" mb="1rem">
+        <Typography mt="4rem" mb="1rem">
           Lists also support various operations such as concatenation,
           repetition, and membership testing using <code>+</code>,{' '}
           <code>*</code>, and <code>in</code> respectively. Examples are given
           below:
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '4rem',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`fruits = ["apple", "banana"]
+        <CodeEditorWithOutput
+          content={`fruits = ["apple", "banana"]
 
-print(fruits + ["cherry", "orange"])  # merges the 2 lists
-# Output: ['apple', 'banana', 'cherry', 'orange']
+# merges the 2 lists
+print("Using + with Lists:")
+print(fruits + ["cherry", "orange"])
+print() 
 
-print(fruits * 3) # multiplies the items in the list n times
-# Output: ['apple', 'banana', 'apple', 'banana', 'apple', 'banana']
+# multiplies the items in the list n times
+print("Using * with Lists:")
+print(fruits * 3)
+print()
 
-print("apple" in fruits)  # checks if the element "apple" is in the fruits list
-# Output: True
+# checks if the element "apple" is in the fruits list
+print("Using 'in' with Lists:")
+print("apple" in fruits)
+print()  
+
 `}
-            height="320px"
-            width="100%"
-            title="true"
-          />
+          codeWidth={60}
+          outputWidth={40}
+          height="520px"
+          title="true"
+        />
 
-          <CodeOutput
-            content={`['apple', 'banana', 'cherry', 'orange']
-['apple', 'banana', 'apple', 'banana', 'apple', 'banana']
-True
-            `}
-            height="200px"
-            width="100%"
-            title="true"
-          />
-        </Box>
-
-        <Typography mb="2rem">
+        <Typography mt="4rem" mb="2rem">
           Now, apply your skills in the activity below!
         </Typography>
 
