8af6cb19cf178613f91c23832b75e46768bfd683
Chav Sumalinog <chaviotics@gmail.com>
2023-05-26 16:49:54 +0800

    Updated conditional statement page to use interactive code editors

--- client/src/pages/ConditionalStatements/ConditionalStatements.jsx
+++ client/src/pages/ConditionalStatements/ConditionalStatements.jsx
@@ -67,100 +67,71 @@ function ConditionalStatements() {
 
         {/* 1st Example */}
 
-        <Typography>For example:</Typography>
+        <Typography mb="2rem">
+          Press <code>RUN CODE</code> in the interactive code editor below!
+        </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '2rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`age = 20
+        <Typography mb="1rem">For example:</Typography>
+
+        <CodeEditorWithOutput
+          content={`# Try changing the value of "age"!
+age = 20
+
 if age >= 18:
-    print("You are old enough to vote.")`}
-            height="150px"
-            width="60%"
-            title="true"
-          />
-          <CodeOutput
-            content={`You are old enough to vote.`}
-            height="150px"
-            width="40%"
-            title="true"
-          />
-        </Box>
+    print("You are old enough to vote.")
+`}
+          codeWidth={60}
+          outputWidth={40}
+          height="200px"
+          title="true"
+        />
 
-        <Typography mb="4rem">
+        <Typography mt="2rem" mb="4rem">
           In the example above, the <code>condition</code>{' '}
           <code>age &gt;= 18</code> checks if the value of <code>age</code> is
           greater than or equal to 18. If it is, the code inside the{' '}
           <code>if</code> statement will be executed and the message "You are
-          old enough to vote." will be printed.
+          old enough to vote." will be printed.{' '}
+          <em>Check out what happens when age is less than 18!</em>
         </Typography>
 
         {/* 2nd Example */}
-        <Typography mt="1rem">
+        <Typography mt="2rem" mb="1rem">
           You can also use an <code>else</code> statement to specify what should
           happen if the <code>condition</code> is <code>False</code>:
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '2rem',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-          }}
-        >
-          {/* <CodeExample2 />
-          <CodeOutput2 /> */}
-          <CodeEditor
-            content={`age = 15
+        <CodeEditorWithOutput
+          content={`# Try changing the value of "age"!
+age = 15
+
 if age >= 18:
     print("You are old enough to vote.")
 else:
     print("You are not old enough to vote.")
 `}
-            height="200px"
-            width="65%"
-            title="true"
-          />
-          <CodeOutput
-            content={`You are not old enough to vote.`}
-            height="200px"
-            width="35%"
-            title="true"
-          />
-        </Box>
+          codeWidth={60}
+          outputWidth={40}
+          height="240px"
+          title="true"
+        />
 
-        <Typography mb="4rem">
+        <Typography mt="2rem" mb="4rem">
           In the example above, if the value of <code>age</code> is greater than
           or equal to 18, the first message will be printed. If not, the second
           message will be printed.
         </Typography>
 
         {/* 3rd Example */}
-        <Typography>
+        <Typography mb="1rem">
           Also, you can even add multiple conditions using <code>elif</code>{' '}
           (Python's version of "else if") statements:
         </Typography>
 
-        <Box
-          sx={{
-            mt: '1rem',
-            mb: '2rem',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '1rem',
-          }}
-        >
-          <CodeEditor
-            content={`age = 20
+        <CodeEditorWithOutput
+          content={`# Try changing the value of "age"!
+age = 20 
+
 if age < 18:
     print("You are a minor.")
 elif age >= 18 and age < 21:
@@ -168,28 +139,29 @@ elif age >= 18 and age < 21:
 else:
     print("You are an adult and can drink.")
 `}
-            height="250px"
-            width="100%"
-            title="true"
-          />
+          codeWidth={60}
+          outputWidth={40}
+          height="320px"
+          title="true"
+        />
 
-          <CodeOutput
-            content={`You are an adult but not old enough to drink.`}
-            height="150px"
-            width="100%"
-            title="true"
-          />
-        </Box>
-
-        <Typography mb="4rem">
+        <Typography mt="2rem" mb="4rem">
           In the example above, the code checks multiple conditions to determine
           the age group of the person. Depending on the result of these
           conditions, different messages will be printed.
         </Typography>
 
-        <Typography fontStyle="italic" mb="4rem">
+        {/* Notes */}
+
+        <Typography fontStyle="italic" mb="1rem">
           In Python, you can use the <code>and</code>, <code>or</code> and{' '}
           <code>not</code> logical operators in conditional statements.
+        </Typography>
+
+        <Typography fontStyle="italic" mb="4rem">
+          Also, you can enclose the conditions using parenthesis <code>()</code>
+          . For example, <code>age == 18</code> and <code>(age == 18)</code> are
+          both valid syntax in Python.
         </Typography>
 
         <Typography mb="2rem">
